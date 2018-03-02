19ccc816a551ed17af7bc9294758bf391e0f4000
Bassim Hawa <s436437@ocsb.local>
2018-03-02 11:05:33 -0500

    yes

--- main.lua
+++ main.lua
@@ -5,10 +5,6 @@
 --This program displays three polygons, as well as their names.
 -----------------------------------------------------------------------------------------
 
---set the (x,y) position of the rounded rectangle as well as it's size
-local roundedRect = display.newRoundedRect( 120, 50, 200, 50, 10 )
-roundedRect.path.radius = 20
-
 local textSize = 25
 local areaText
 
@@ -18,6 +14,10 @@ display.setDefault("background", 0/255, 100/255, 0/255)
 --to remove status bar
 display.setStatusBar(display.HiddenStatusBar)
 
+--set the (x,y) position of the rounded rectangle as well as its size, and radius of the circular corners
+local roundedRect = display.newRoundedRect( 120, 50, 200, 50, 10 )
+roundedRect.path.radius = 20
+
 --anchor the rounded reectangle in the top left corner
 roundedRect.anchorX = 0
 roundedRect.anchorY = 0
@@ -25,7 +25,7 @@ roundedRect.x = 20
 roundedRect.y = 20
 
 --set the width of the border
-roundedRect.strokeWidth = 15
+roundedRect.strokeWidth = 10
 
 --set the color of the rounded rectangle
 roundedRect:setFillColor(0.5, 0.5, 1)
@@ -40,4 +40,16 @@ areaText = display.newText("Rounded Rectangle", 0, 0, Arial, textSize)
 areaText.anchorX = 0
 areaText.anchorY = 0
 areaText.x = 20
-areaText.y = 100
+areaText.y = 90
+
+--create a triangle using lines
+local triangle = display.newLine(200, 90, 227, 165 )
+triangle:append(173,165, 200,90 )
+triangle:setStrokeColor( 1, 0, 0, 1 )
+triangle.strokeWidth = 8
+
+--anchor the traiangle in the top left corner
+triangle.anchorX = 0
+triangle.anchorY = 0
+triangle.x = 400
+triangle.y =20
